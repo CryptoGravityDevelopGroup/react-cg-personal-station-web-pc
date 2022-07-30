f8481089b88d5499ca950381c355e2a56c30491a
zhaobowen <1964471757@qq.com>
2022-07-30 17:36:23 +0800

    delet lock

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // let baseUrl = "http://18.144.74.181:26888";
-let baseUrl = "http://localhost:3000/";
+// let baseUrl = "http://localhost:3000/";
+let baseUrl = "https://207c1p0569.51mypc.cn/token/list";
 const service = axios.create({
   baseURL: baseUrl,
 });
