03513adc4ae8f3fa6ecee607fbaa089e7ef7f306
zhaobowen <1964471757@qq.com>
2022-08-04 23:59:11 +0800

    修改地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // let baseUrl = "http://localhost:3000/";
-let baseUrl = "https://207c1p0569.51mypc.cn";
-// let baseUrl = "http://18.144.74.181:26888";
+// let baseUrl = "https://207c1p0569.51mypc.cn";
+let baseUrl = "http://18.144.74.181:26888";
 const service = axios.create({
   baseURL: baseUrl,
 });
